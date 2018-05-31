2c235f74f156bcd0e2c04929c7fb69d3a74fe2b5
bllxk <bllxk@hotmail.com>
2018-05-31 16:00:09 +0800

    0.1

---
+++ Shebao/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 使得变量可以被Inspector界面获得
+[System.Serializable]
+public class Player_Boundary 
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float tilt;
+	public Player_Boundary boundary;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	 
+	private float nextFire;
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetButton("Fire1") && Time.time > nextFire) 
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+			GetComponent<AudioSource>().Play ();
+		}
+	}
+
+
+}
